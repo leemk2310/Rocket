194049d3eddfc27d78aa8b93629399d532ee633a
leemk2310 <lee.minhkhanh@gmail.com>
2023-07-19 18:47:12 +0900

    Test
    ww

--- Assets/Scrip/Occillator.cs
+++ Assets/Scrip/Occillator.cs
@@ -16,7 +16,7 @@ public class Occillator : MonoBehaviour
 
     void Update()
     {
-        
+        if (period <= Mathf.Epsilon) { return; }
         float cycles = Time.time / period;  // continually growing over time
         
         const float tau = Mathf.PI * 2;  // constant value of 6.283
